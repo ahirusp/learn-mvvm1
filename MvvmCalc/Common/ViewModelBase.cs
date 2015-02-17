e034a249f93f262a9c42ca7b43f8d78ffc260302
miyake <ako.miyake@gmail.com>
2015-02-17 14:48:29 +0900

    IDataErrorInfoインターフェースを実�

--- MvvmCalc/Common/ViewModelBase.cs
+++ MvvmCalc/Common/ViewModelBase.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace MvvmCalc.Common
 {
     /// <summary>
     /// ViewModelの基本クラス。INotifyPropertyChangedの実装を提供します。
     /// </summary>
-    public class ViewModelBase : INotifyPropertyChanged
+    public class ViewModelBase : INotifyPropertyChanged, IDataErrorInfo
     {
         /// <summary>
         /// プロパティの変更があった時に発行されます。
@@ -19,6 +20,71 @@ namespace MvvmCalc.Common
             {
                 h(this, new PropertyChangedEventArgs(propertyName));
             }
+        }
+
+        /// <summary>
+        /// プロパティに紐づいたエラーメッセージを返します。
+        /// </summary>
+        private Dictionary<string, string> errors = new Dictionary<string, string>();
+
+        /// <summary>
+        /// columnNameで指定したプロパティのエラーを返します。
+        /// </summary>
+        /// <param name="columnName">プロパティ名</param>
+        /// <returns>エラーメッセージ</returns>
+        public string this[string columnName]
+        {
+            get
+            {
+                if (this.errors.ContainsKey(columnName))
+                {
+                    return this.errors[columnName];
+                }
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// プロパティにエラーメッセージを設定します。
+        /// </summary>
+        /// <param name="propertyName">プロパティ名</param>
+        /// <param name="errorMessage">エラーメッセージ</param>
+        protected void SetError(string propertyName, string errorMessage)
+        {
+            this.errors[propertyName] = errorMessage;
+            this.RaisePropertyChanged("HasError");
+        }
+
+        /// <summary>
+        /// プロパティのエラーをクリアします。
+        /// </summary>
+        /// <param name="propertyName">プロパティ名</param>
+        protected void ClearError(string propertyName)
+        {
+            if (this.errors.ContainsKey(propertyName))
+            {
+                this.errors.Remove(propertyName);
+                this.RaisePropertyChanged("HasError");
+            }
+        }
+
+        /// <summary>
+        /// すべてのエラーをクリアします。
+        /// </summary>
+        protected void ClearErrors()
+        {
+            this.errors.Clear();
+            this.RaisePropertyChanged("HasError");
+        }
+
+        public bool HasError
+        {
+            get { return this.errors.Count != 0; }
+        }
+
+        public string Error
+        {
+            get { throw new System.NotImplementedException(); }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ﻿MVVM入門 その１「シンプル四則演算アプリケーションの作成」 - Dev Center
 https://code.msdn.microsoft.com/windowsdesktop/MVVM-d8261534
+
+MVVM入門 その２「入力値の検証」
+https://code.msdn.microsoft.com/windowsdesktop/MVVM-850a376f
