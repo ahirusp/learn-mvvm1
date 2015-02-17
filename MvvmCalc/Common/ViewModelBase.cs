20b489bef9d893092ab6cfcf1a5c2f4215e0394d
miyake <ako.miyake@gmail.com>
2015-02-17 11:36:19 +0900

    基本クラスを追加

---
+++ MvvmCalc/Common/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace MvvmCalc.Common
+{
+    /// <summary>
+    /// ViewModelの基本クラス。INotifyPropertyChangedの実装を提供します。
+    /// </summary>
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// プロパティの変更があった時に発行されます。
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            var h = this.PropertyChanged;
+            if (h != null)
+            {
+                h(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
