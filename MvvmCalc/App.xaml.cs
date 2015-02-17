7840b35911386a3b6d2988b0603ae43d52ae5f8e
miyake <ako.miyake@gmail.com>
2015-02-17 14:14:04 +0900

    Viewを修正 起動時の処理を追加

--- MvvmCalc/App.xaml.cs
+++ MvvmCalc/App.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using MvvmCalc.View;
 
 namespace MvvmCalc
 {
@@ -13,5 +8,10 @@ namespace MvvmCalc
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            var v = new MainView();
+            v.Show();
+        }
     }
 }
